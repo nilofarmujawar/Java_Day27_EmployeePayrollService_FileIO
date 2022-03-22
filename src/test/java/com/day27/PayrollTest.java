f5d383558a53285438d7bd9ff91bb94e96c0d694
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-03-23 00:25:25 +0530

    Added: method to read from the file UC6

--- src/test/java/com/day27/PayrollTest.java
+++ src/test/java/com/day27/PayrollTest.java
@@ -8,12 +8,7 @@ import java.util.Arrays;
 
 @SuppressWarnings("deprecation")
 public class PayrollTest {
-    @Test
-    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
-        EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, 25000.56, "Devnandan Kumar"),
-                new EmployeePayrollData(2, 15000.65, "Manoj Kumar"),
-                new EmployeePayrollData(3, 22000.67, "Abhishek sinha") };
-        };
+
     @Test
     public void given3EmployeesWhenWrittenToFileShouldMatchEmployeesEntries() {
         EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, 25000.56, "Devnandan Kumar"),
@@ -25,6 +20,12 @@ public class PayrollTest {
         long entriesCount=EmployeePayrollService.countEntries(com.day27.EmployeePayrollService.IOService.FILE_IO);
         EmployeePayrollService.printData(com.day27.EmployeePayrollService.IOService.FILE_IO);
         Assertions.assertEquals(3, entriesCount);
+    }
+
+    @Test
+    public void givenFileOnReadingFromFileShouldMatchEmployeeCount() {
+        EmployeePayrollService payrollService=new EmployeePayrollService();
+        long entries=payrollService.readEmployeeData(EmployeePayrollService.IOService.FILE_IO);
     }
 
 }
